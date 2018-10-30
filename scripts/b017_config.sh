8b99a2ecb10babe1f9e08333768f86a2c74a0f32
Boryana S. Koseva <boryana@ku.edu>
2018-10-30 12:17:55 -0500

    added two subdirs in reports

--- scripts/b017_config.sh
+++ scripts/b017_config.sh
@@ -4,7 +4,8 @@
 KINBRE_DIR=/panfs/pfs.local/scratch/sjmac/b961k922/kinbre
 
 PROJECT="b017"
-PROJECT_DIR=${KINBRE_DIR}/b017_macdonald && cd ${PROJECT_DIR}
+PROJECT_DIR=${KINBRE_DIR}/b017_macdonald
+cd ${PROJECT_DIR} || { echo "could not cd into ${PROJECT_DIR}"; exit 1; }
 REF_GENOME=${PROJECT_DIR}/refs/dmel_main_chr_r6.03_masked.fa
 
 ## organize the folders for the data
@@ -24,15 +25,24 @@ HR_DATA_FILT=${DATA_DIR}/intercross_hr/filtered
 
 # prepare the output directories
 OUTPUTS_DIR=${PROJECT_DIR}/outputs
-PARENTS_ALN_DIR=${PROJECT_DIR}/outputs/aln/parental_lines
-F18_ALN_DIR=${PROJECT_DIR}/outputs/aln/intercross_f18
-HR_ALN_DIR=${PROJECT_DIR}/outputs/aln/intercross_hr
 
+PARENTS_ALN_DIR=${OUTPUTS_DIR}/aln/parental_lines
+PARENTS_SNPS_DIR=${OUTPUTS_DIR}/variants/parental_lines
+
+F18_ALN_DIR=${OUTPUTS_DIR}/aln/intercross_f18
+F18_SNPS_DIR=${OUTPUTS_DIR}/variants/intercross_f18
+
+HR_ALN_DIR=${OUTPUTS_DIR}/aln/intercross_hr
+HR_SNPS_DIR=${OUTPUTS_DIR}/variants/intercross_hr
+
+
+REPORTS_DIR=${PROJECT_DIR}/report_files
+ALN_REPORTS_DIR=${REPORTS_DIR}/aln
+SNPS_REPORTS_DIR=${REPORTS_DIR}/var_calling
 
 SCRIPTS_DIR=${PROJECT_DIR}/scripts
 INFO_DIR=${PROJECT_DIR}/info
 REFS_DIR=${PROJECT_DIR}/refs
-REPORTS_DIR=${PROJECT_DIR}/report_files
 
 # create the dirs if they don't already exist
 mkdir -p ${PROJECT_DIR} \
@@ -46,8 +56,13 @@ mkdir -p ${PROJECT_DIR} \
          ${SCRIPTS_DIR} \
          ${OUTPUTS_DIR} \
          ${PARENTS_ALN_DIR} \
+         ${PARENTS_SNPS_DIR} \
          ${F18_ALN_DIR} \
+         ${F18_SNPS_DIR} \
          ${HR_ALN_DIR} \
+         ${HR_SNPS_DIR} \
          ${INFO_DIR} \
          ${REFS_DIR} \
-         ${REPORTS_DIR}
+         ${REPORTS_DIR} \
+         ${ALN_REPORTS_DIR} \
+         ${SNPS_REPORTS_DIR}
