c2ecb0bb518fbc8f749490592687c9499b288d62
Boryana S. Koseva <boryana@ku.edu>
2018-10-31 20:17:47 -0500

    bcf parents with default parameters

--- scripts/b017_bcf_parents.sh
+++ scripts/b017_bcf_parents.sh
@@ -9,7 +9,13 @@
 echo "Sourcing the config file"
 source /panfs/pfs.local/scratch/sjmac/b961k922/kinbre/b017_macdonald/scripts/b017_config.sh
 
+while [[ ! -f "${PROJECT_DIR}/status/parents.align.done" ]]; do
+  sleep 1m
+done
+
+
 parental_lines=( Db3852 Dsam )
+N_THREADS=8
 
 activate_conda_environment () {
   ENVNAME=${1}_${2}
@@ -30,8 +36,23 @@ activate_conda_environment () {
 
 activate_conda_environment ${PROJECT} bcftools
 
-bcftools mpileup -Ou -f ${REFGENOME} \
-                 ${ALNDIR}/${parental_lines[0]}.sorted.bam \
-                 ${ALNDIR}/${parental_lines[1]}.sorted.bam | \
-                 bcftools call -vmO z -o ${ALNDIR}/${parent}.vcf.gz || { echo "bcftools failed" ; exit 1; }
-source deactivate
+OUTPUT_PREFIX=${parental_lines[0]}_${parental_lines[1]}
+
+bcftools mpileup -Ob -o ${PARENTS_SNPS_DIR}/${OUTPUT_PREFIX}.bcf \
+                 --threads $(( ${N_THREADS} - 1 )) \
+                 -f ${REF_GENOME} \
+                 ${PARENTS_ALN_DIR}/${parental_lines[0]}.sorted.bam \
+                 ${PARENTS_ALN_DIR}/${parental_lines[1]}.sorted.bam || { echo "bcftools mpileup failed" ; exit 1; }
+
+bcftools call -vmO z --threads $(( ${N_THREADS} - 1 )) \
+              -o ${PARENTS_SNPS_DIR}/${OUTPUT_PREFIX}.vcf.gz \
+              ${PARENTS_SNPS_DIR}/${OUTPUT_PREFIX}.bcf || { echo "bcftools call failed" ; exit 1; }
+
+tabix -p vcf ${PARENTS_SNPS_DIR}/${OUTPUT_PREFIX}.vcf.gz || { echo "tabix failed" ; exit 1; }
+
+bcftools stats -F ${REF_GENOME} \
+               -s - ${PARENTS_SNPS_DIR}/${OUTPUT_PREFIX}.vcf.gz > ${PARENTS_SNPS_DIR}/${OUTPUT_PREFIX}.vcf.gz.stats || { echo "bcftools stats failed" ; exit 1; }
+
+mkdir ${PARENTS_SNPS_DIR}/plots || { echo "failed to created dir: ${PARENTS_SNPS_DIR}/plots" ; exit 1; }
+
+plot-vcfstats -p ${PARENTS_SNPS_DIR}/plots ${PARENTS_SNPS_DIR}/${OUTPUT_PREFIX}.vcf.gz.stats || { echo "plot-vcfstats failed" ; exit 1; }
